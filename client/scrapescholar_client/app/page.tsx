9c9855a63c43491d66995ba325a580d91b3fc105
beedawn <brandonschmersal@gmail.com>
2024-08-29 10:31:25 -0400

    add delete input button for UI

--- client/scrapescholar_client/app/page.tsx
+++ client/scrapescholar_client/app/page.tsx
@@ -1,41 +1,59 @@
 "use client";
 import Image from "next/image";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
 import SearchBox from "./components/SearchBox";
 export default function Home() {
-  const [results, setResults]= useState('');
-  const [inputs, setInputs]= useState(['']);
-  const addInput = () =>{
-      setInputs([...inputs,'']);
+  const [results, setResults] = useState('');
+  const [inputs, setInputs] = useState(['']);
+  const [displayInputs, setDisplayInputs] = useState<string[]>([]);
+
+  const addInput = () => {
+    setInputs([...inputs, '']);
+  }
+  const removeInput = (index: number) => {
+    const newInput = inputs.filter((_,input_index)=> input_index !== index)
+    setInputs([...newInput]);
   }
 
-  const handleSearchChange = (index:number, e: React.ChangeEvent<HTMLTextAreaElement>) => {
-const newInputs = [...inputs];
- newInputs[index] =e.target.value;
- setInputs(newInputs);
+
+  const handleSearchChange = (index: number, e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newInputs = [...inputs];
+    newInputs[index] = e.target.value;
+    setInputs(newInputs);
+
   }
 
-  const handleResults = () =>{
+  const handleResults = () => {
     //run off and get results somewhere
-      setResults('No results found.')
+    setResults('No results found.');
+    const filteredInputs = [...inputs].filter((input)=>{return(input)}).join(', ')
+    setDisplayInputs([filteredInputs]);
   }
-  return ( 
+  return (
     <div>
-    <div style={{maxWidth:"20%", padding:"50px", marginRight:"auto", float:"left"}}>
-        <Button children="+" onClick={addInput} className="m-5"/>
+      <div style={{ maxWidth: "20%", padding: "50px", marginRight: "auto", float: "left" }}>
+        <Button children="+" onClick={addInput} className="m-5" />
         <Button children="Search" onClick={handleResults} />
-    {inputs.map((input, index) =>{
-      return(<div  key={index}>
- <SearchBox value={input} onChange={(e)=>{handleSearchChange(index, e)}}/>
- 
- </div>)
-    })}
- </div>
-    <div style = {{maxWidth:"80%", padding:"50px", float:"left"}}>
-      {results}
+        {inputs.map((input, index) => {
+          return (<div key={index}>
+            <SearchBox value={input} onChange={(e) => { handleSearchChange(index, e) }} />
+            <Button children="-" onClick={() =>{removeInput(index)} } className="m-1 text-sm"  />
+          </div>)
+        })}
       </div>
-   
+      <div style={{ maxWidth: "80%", padding: "50px", float: "left" }}>
+        {results !== '' && (
+          <>
+            <p>You searched {
+              displayInputs
+            }
+            </p>
+          </>)}
+        <div> {results}</div>
+
+      </div>
+
     </div>
   );
 }
