2d6e63b3d07ac9370892c8faaf717908a131d113
beedawn <brandonschmersal@gmail.com>
2024-08-29 12:12:37 -0400

    fix some css styling for inputs

--- client/scrapescholar_client/app/page.tsx
+++ client/scrapescholar_client/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         <Button type="submit" >Search</Button>
         {inputs.map((input, index) => {
           return (<div key={index}>
-            <SearchBox value={input} onChange={(e) => { handleSearchChange(index, e) }} />
+            <SearchBox value={input} onChange={(e) => { handleSearchChange(index, e) }} className="m-2 px-2 py-2 " />
             {index !=0 &&<Button onClick={() =>{removeInput(index)} } className="m-1 text-sm">-</Button>}
           </div>)
         })}
