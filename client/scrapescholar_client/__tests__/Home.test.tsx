9d4946dc3d89853ca8bacaa1e9f59b00a6de913b
beedawn <brandonschmersal@gmail.com>
2024-08-29 11:01:14 -0400

    add comments to tests

--- client/scrapescholar_client/__tests__/Home.test.tsx
+++ client/scrapescholar_client/__tests__/Home.test.tsx
@@ -8,20 +8,32 @@ describe('Home Component', () => {
 
   test('check + button loads', () => {
     render(<Home />);
+    //finds + button
     const addButton = screen.getByText('+');
     expect(addButton).toBeInTheDocument(); 
   });
 
   test('check SearchBox loads', () => {
     render(<Home />);
+    //checks how many input boxes there are 
     expect(screen.getAllByRole('textbox')).toHaveLength(1); 
+  });
+
+  test('check - button loads', () => {
+    render(<Home />);
+    //finds - button
+    const deleteButton = screen.getByText('-');
+    expect(deleteButton).toBeInTheDocument(); 
   });
 
   test('adds new input field on "+" button click', () => {
     render(<Home />);
     const addButton = screen.getByText('+');
+    //clicks + button
     fireEvent.click(addButton);
+    //should be 2 inputs now
     expect(screen.getAllByRole('textbox')).toHaveLength(2); 
+    //maybe should test there are two + buttons too?
   });
 
   test('updates input value correctly', () => {
@@ -36,8 +48,30 @@ describe('Home Component', () => {
     const searchButton = screen.getByText('Search');
     fireEvent.click(searchButton);
     expect(screen.getByText('No results found.')).toBeInTheDocument()
-
-
-
   })
+
+  test('shows you searched test input after search press', () =>{
+    render(<Home />);
+    const inputs = screen.getAllByRole('textbox');
+    fireEvent.change(inputs[0], { target: { value: 'test input' } });
+    const searchButton = screen.getByText('Search');
+    fireEvent.click(searchButton);
+    expect(screen.getByText('You searched test input')).toBeInTheDocument()
+  })
+
+  test('removes input when delete button clicked', () => {
+    render(<Home />);
+    const addButton = screen.getByText('+');
+    const deleteButton = screen.getAllByText('-');
+    fireEvent.click(addButton);
+    const inputs = screen.getAllByRole('textbox');
+    const firstInput = inputs[0]
+    const secondInput = inputs[1]
+    const firstDeleteButton = deleteButton[0]
+    fireEvent.change(firstInput, { target: { value: 'test input' } });
+    fireEvent.change(secondInput, { target: { value: 'test input 2' } });
+    fireEvent.click(firstDeleteButton);
+    expect(screen.getAllByRole('textbox')).toHaveLength(1); 
+    expect(screen.getAllByRole('textbox')[0]).toHaveValue('test input 2')
+  });
 });
