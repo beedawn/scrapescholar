4963c35a22adac60a0d20bf089846ac3deb2322e
beedawn <bee@macbook-pro.909.vc>
2024-09-02 19:59:20 -0400

    convert classes

--- client/scrapescholar_client/app/components/NavBar.tsx
+++ client/scrapescholar_client/app/components/NavBar.tsx
@@ -1,49 +1,47 @@
-import React,  {  Dispatch,SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import Button from './Button';
 import SearchBox from './SearchBox';
 import DropdownSearchBox from './DropdownSearchBox';
 import Dropdown from '../types/DropdownType';
 interface NavBarProps {
-    children?: React.ReactNode;
-    onClick?: () => void;
-    className?: string;
-    type?: any;
     handleResults: (event: React.FormEvent<HTMLFormElement>) => void;
     addInput: () => void;
     inputs: string[];
-    handleSearchChange: (index: number, event: React<HTMLInputElement>) => void;
-    handleDropdownChange: (index:number, option:Dropdown)=> void;
+    handleSearchChange: (index: number, event: React.ChangeEvent<HTMLInputElement>) => void;
+    handleDropdownChange: (index: number, option: Dropdown) => void;
     removeInput: (index: number) => void;
-    setLoggedIn:Dispatch<SetStateAction<boolean>>;
-    dropdown:Dropdown[];
+    setLoggedIn: Dispatch<SetStateAction<boolean>>;
+    dropdown: Dropdown[];
 }
 
-const NavBar: React.FC<NavBarProps> = ({ children, onClick, className, handleResults,
+const NavBar: React.FC<NavBarProps> = ({ handleResults,
     addInput, inputs, handleSearchChange, removeInput, setLoggedIn, dropdown, handleDropdownChange }) => {
-
-        const handleLogout = () => {
-            setLoggedIn(false); // Set the loggedIn state to false
-          };
+    const handleLogout = () => {
+        setLoggedIn(false);
+    };
     return (
         <>
-            <div style={{ maxWidth: "400px", padding: "50px", marginRight: "auto", float: "left" }}>
-            <Button onClick={handleLogout} className="">Logout</Button>
+            <div className="p-5 max-w-md mr-auto float-left">
+                <div className="float-right pb-6" >
+                    <Button onClick={handleLogout} className="">Logout</Button>
+                </div>
                 <h1 className="text-4xl font-bold">ScrapeScholar</h1>
                 <form onSubmit={handleResults}>
                     <Button onClick={addInput} className="m-5">+</Button>
                     <Button type="submit" >Search</Button>
                     {inputs.map((input: string, index: number) => {
                         return (<div key={index}>
-                            <SearchBox value={input} onChange={(e) => { handleSearchChange(index, e) }} className="m-2 px-2 py-2 " />
-                         {index != 0
+                            <SearchBox value={input} onChange={(e) => { handleSearchChange(index, e) }} className="m-2" />
+                            {index != 0
                                 &&
                                 <Button onClick={() => {
                                     removeInput(index)
                                 }} className="m-1 text-sm bg-red-600">
                                     -
                                 </Button>}<br />
-                                {inputs.length > 1 && index != inputs.length-1 && inputs[index].length>0 &&<DropdownSearchBox value={dropdown[index]} className="p-2" onDropdownChange={(e)=>{handleDropdownChange(index, e.target.value as Dropdown)}}></DropdownSearchBox>}
-                           
+                            {inputs.length > 1 && index != inputs.length - 1 && inputs[index].length > 0 &&
+                                <DropdownSearchBox value={dropdown[index]} className="ml-2" onDropdownChange={(e) => { handleDropdownChange(index, e.target.value as Dropdown) }}></DropdownSearchBox>}
+
                         </div>)
                     })}
                 </form>
