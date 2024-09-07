d30af9e94a680317a4d232cd06ba8e65b1140eb5
beedawn <bee@MacBook-Pro.local>
2024-09-07 19:20:11 -0400

    science direct api list

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
-import backend.sciencedirect
+import backend.sciencedirect.sciencedirect as sciencedirect
 
 app = FastAPI()
-
 
 @app.get("/health_check")
 async def health_check():
     return {"message": "Hello World"}
 
 
-
 @app.get("/sciencedirect")
 async def get_sciencedirect_data():
-    result=backend.sciencedirect.request_api()
+    result=sciencedirect.request_api()
     return result
