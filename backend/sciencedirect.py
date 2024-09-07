d0566adec2adde38495711ab0022877670fb2ef2
beedawn <bee@MacBook-Pro.local>
2024-09-07 19:07:02 -0400

    science direct relaying api to /sciencedirect endpoint

--- backend/sciencedirect.py
+++ backend/sciencedirect.py
@@ -1,10 +1,16 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Access an environment variable
+api_key = os.getenv('SCIENCEDIRECT_APIKEY')
 
 def request_api():
-    x = requests.get('')
-    print(x.status_code)
-    print(x.json())
-    return {"sciencedirect":"message"}
+
+    response = requests.get(f"https://api.elsevier.com/content/search/sciencedirect?query=gene&apiKey={api_key}")
+
+    return response.json()
