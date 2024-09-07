9344cd9c29cce38fcb55febb0694c3c135dc18c2
beedawn <bee@MacBook-Pro.local>
2024-09-07 18:54:31 -0400

    testing env git ignore

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import backend.sciencedirect
 
 app = FastAPI()
 
 
 @app.get("/health_check")
-async def root():
+async def health_check():
     return {"message": "Hello World"}
+
+
+
+@app.get("/sciencedirect")
+async def get_sciencedirect_data():
+    result=backend.sciencedirect.request_api()
+    return result
