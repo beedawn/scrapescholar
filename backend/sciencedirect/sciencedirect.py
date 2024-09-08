f1ea47c025716c96df444ec216a31db7771444e3
beedawn <bee@MacBook-Pro.local>
2024-09-07 21:15:35 -0400

    add query string to sciencedirect endpoint

--- backend/sciencedirect/sciencedirect.py
+++ backend/sciencedirect/sciencedirect.py
@@ -10,9 +10,9 @@ api_key = os.getenv('SCIENCEDIRECT_APIKEY')
 #keyword for search
 keyword = "cybersecurity"
 # triggers for science direct endpoint
-def request_api():
+def request_api(query: str):
     #request data from science direct
-    response = requests.get(f"https://api.elsevier.com/content/search/sciencedirect?query={keyword}&apiKey={api_key}")
+    response = requests.get(f"https://api.elsevier.com/content/search/sciencedirect?query={query}&apiKey={api_key}")
     #get json data
     data = response.json()
     #get search-results object
