d4901fc27333bcbf423eb0e8d7d78e6151f367e5
beedawn <brandonschmersal@gmail.com>
2024-09-09 13:24:36 -0400

    testing again

---
+++ backend/tests/integration/test_main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/health_check")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
