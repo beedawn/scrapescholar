9663de0262e2eb90a217d49ab676d147055d179f
beedawn <brandonschmersal@gmail.com>
2024-09-09 07:38:35 -0400

    filter data for science direct

--- backend/sciencedirect/sciencedirect.py
+++ backend/sciencedirect/sciencedirect.py
@@ -1,13 +1,13 @@
 import requests
 import os
 from dotenv import load_dotenv
-
+ 
 # Load environment variables from .env file
-load_dotenv()
-
+load_dotenv()            
+                         
 # Access an environment variable
 api_key = os.getenv('SCIENCEDIRECT_APIKEY')
-#keyword for search
+#keyword for search      
 keyword = "cybersecurity"
 # triggers for science direct endpoint
 def request_api(query: str):
@@ -20,4 +20,20 @@ def request_api(query: str):
     #get entries list
     entries = search_results.get('entry',[])
     #return entries to sciencedirect endpoint response
-    return entries
+ 
+    returnList = []
+ 
+    for entry in entries:
+        title = entry.get('dc:title')
+        links = entry.get('link')
+        link = links[1].get('@href')
+        returnList.append({
+            'title':title, 
+            'link':link
+            })
+ 
+    return returnList
+
+
+
+
