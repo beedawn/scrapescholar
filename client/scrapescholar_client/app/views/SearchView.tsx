20703d7e1ca73fb9374f4c079a09030a4c2e20dc
beedawn <bee@macbook-pro.909.vc>
2024-09-10 19:27:02 -0400

    update

--- client/scrapescholar_client/app/views/SearchView.tsx
+++ client/scrapescholar_client/app/views/SearchView.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState, Dispatch,SetStateAction, } from 'react';
+import React, { useState, Dispatch, SetStateAction, } from 'react';
 import SearchResults from "../components/SearchResults";
 import NavBar from "../components/NavBar";
 import Dropdown from "../types/DropdownType";
@@ -11,100 +11,127 @@ interface SearchViewProps {
     disableD3?: boolean;
 }
 
-export interface ResultItem{
-    id:number;
-    title:string;
-    link:string;
-    date:string;
-    source:string;
+export interface ResultItem {
+    id: number;
+    title: string;
+    link: string;
+    date: string;
+    source: string;
 }
+
 const SearchView: React.FC<SearchViewProps> = ({ setLoggedIn, disableD3 = false }) => {
+    //gets data from api and stores in results
     const [results, setResults] = useState<ResultItem[]>([]);
+    //inputs gets user inputs, update everytime user enters character
     const [inputs, setInputs] = useState<string[]>(['']);
-    const [bubbleInputs, setBubbleInputs] = useState<{ x: number, y: number, radius: number, color: string, label: string }[]>([]);
+    //bubble inputs is passed to bubble plot, pure inputs that update when Search is pressed only
+    const [bubbleInputs, setBubbleInputs] = useState<{ 
+        x: number, 
+        y: number, 
+        radius: number, 
+        color: string, 
+        label: string 
+    }[]>([]);
+    //string of inputs joined with ' '
     const [joinedInputsString, setJoinedInputsString] = useState<string[]>([]);
+    //drop down array for dropdown values
     const [dropdown, setDropdown] = useState<Dropdown[]>([Dropdown.AND]);
-    const [loading, setLoading]=useState<boolean>(false);
+    //triggers when search is pressed so that UI is updated to loading
+    const [loading, setLoading] = useState<boolean>(false);
+
+    //empty string variable to make code easier to read
     const emptyString = '';
+
+    //adds input and drop down when plus is pressed
     const addInput = () => {
         setInputs([...inputs, emptyString]);
         setDropdown([...dropdown, Dropdown.AND]);
     }
+    //removes input when minus is pressed
     const removeInput = (index: number) => {
         const newInput = inputs.filter((_, input_index) => input_index !== index)
         setInputs([...newInput]);
     }
+    //updates inputs when input field is edited
     const handleSearchChange = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
         const newInputs = [...inputs];
         newInputs[index] = e.target.value;
         setInputs(newInputs);
     }
+    //updates data in dropdown array when and/or/not is selected
     const handleDropdownChange = (index: number, option: Dropdown) => {
         const newDropdown = [...dropdown];
         newDropdown[index] = option;
         setDropdown(newDropdown);
     }
-   const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    //runs when search is pressed
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+        //prevents default form submit which causes page to reload
         event.preventDefault();
+        //sets loading to true which triggers "Loading" to show in UI
         setLoading(true);
-        const filterBlankInputs = inputs.filter((input)=>(input!==''))
-        console.log(filterBlankInputs)
+        //filters out empty input fields
+        const filterBlankInputs = inputs.filter((input) => (input !== ''))
+        //declare empty array to combien user inputs and values from drop downs
         let inputsAndLogicalOperators: string[] = [];
+        //iterate through valid non blank inputs and append them and associated logical operator
         for (let i = 0; i < filterBlankInputs.length; i++) {
+            //add input value
             inputsAndLogicalOperators.push(filterBlankInputs[i]);
-            if (i < filterBlankInputs.length - 1 && filterBlankInputs[i] != emptyString && filterBlankInputs.length > 1) {
+            //make sure we aren't at end of array
+            if (i < filterBlankInputs.length - 1 
+                // make sure item isn't an empty string, do we need this?
+                // && filterBlankInputs[i] != emptyString
+                //make sure there is more than 1 item in the inputs, if there is only one item we don't need a logical operator 
+                && filterBlankInputs.length > 1) {
+                    //add logical operator under input field to array
                 inputsAndLogicalOperators.push(dropdown[i])
             }
         }
         const inputsAndLogicalOperatorsString = inputsAndLogicalOperators.join(' ')
         setJoinedInputsString([inputsAndLogicalOperatorsString]);
-
-
-      
-            const newBubbleInputs = filterBlankInputs.map((input, i) => ({
-              x: i,
-              y: 50,
-              radius: 50,
-              color: "green",
-              label: input
-            }));
-            setBubbleInputs(newBubbleInputs);
-       
-
-        let data:Response;
+        //can probably move this to bubbleplot
+        const newBubbleInputs = filterBlankInputs.map((input, i) => ({
+            x: i,
+            y: 50,
+            radius: 50,
+            color: "green",
+            label: input
+        }));
+        setBubbleInputs(newBubbleInputs);
+        let data: Response;
         let jsonData;
         if (inputsAndLogicalOperators.length === 0)
             setInputs([emptyString])
-        else{
+        else {
             setInputs([...filterBlankInputs])
-            
             const apiQuery = inputsAndLogicalOperators.join('+')
-  
-            try{
-            data = await fetch(`http://localhost:8000/sciencedirect?query=${apiQuery}`)
-            jsonData = await data.json()
+            try {
+                data = await fetch(`http://localhost:8000/sciencedirect?query=${apiQuery}`)
+                jsonData = await data.json()
             }
-            catch(error:any){
-                jsonData = [{"title":error.message,link:''}]
+            catch (error: any) {
+                jsonData = [{ "title": error.message, link: '' }]
             }
         }
-        //run off and get results somewhere
-
-
-        
-       
-        if(jsonData !== undefined && jsonData.length>0)
+        if (jsonData !== undefined && jsonData.length > 0) {
             setResults(jsonData)
-        else
-        //set better error message
+        }
+        else {
+            //set better error message
             setResults([]);
-            setLoading(false);
+        }
+        setLoading(false);
     }
     return (
         <div>
-                <NavBar handleResults={handleSubmit} addInput={addInput} inputs={inputs} 
-                handleSearchChange={handleSearchChange} removeInput={removeInput} setLoggedIn={setLoggedIn} dropdown={dropdown} handleDropdownChange={handleDropdownChange}/>
-                {loading?<p>Loading</p>:<SearchResults displayInputs={joinedInputsString} results={results} emptyString={emptyString} disableD3={disableD3} inputs={inputs} bubbleInputs={bubbleInputs}/>}
+            <NavBar handleResults={handleSubmit} addInput={addInput} inputs={inputs}
+                handleSearchChange={handleSearchChange} removeInput={removeInput} 
+                setLoggedIn={setLoggedIn} dropdown={dropdown} handleDropdownChange={handleDropdownChange} />
+            {loading ? <p>Loading</p> : 
+            <SearchResults displayInputs={joinedInputsString} 
+            results={results} emptyString={emptyString} disableD3={disableD3} 
+            inputs={inputs} bubbleInputs={bubbleInputs} />}
 
         </div>
     );
