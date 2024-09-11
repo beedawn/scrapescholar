8369d86fa0dcb538659b1d6ecb1f80052b8670f6
beedawn <bee@macbook-pro.909.vc>
2024-09-11 18:31:36 -0400

    update next.js title to scrapescholar

--- client/scrapescholar_client/app/page.tsx
+++ client/scrapescholar_client/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from 'react';
+import Head from 'next/head';
 import SearchView from "./views/SearchView";
 export default function Home() {
 const [loggedIn, setLoggedIn]=useState<boolean>(false);
@@ -8,6 +9,7 @@ const [loggedIn, setLoggedIn]=useState<boolean>(false);
 
   return (
     <div>
+     
       {loggedIn?(<SearchView setLoggedIn={setLoggedIn}/>
     ):(
     <><button onClick={()=>{setLoggedIn(true)}} className={"m-5 px-4 py-2 bg-blue-500 text-white rounded "}>Login</button></>
