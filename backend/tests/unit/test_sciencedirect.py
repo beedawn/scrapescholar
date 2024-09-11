bb4512c286ff62a16cdc95d07fef50bb069b9ad8
beedawn <bee@macbook-pro.909.vc>
2024-09-11 17:22:34 -0400

    add US-11 backend tests

--- backend/tests/unit/test_sciencedirect.py
+++ backend/tests/unit/test_sciencedirect.py
@@ -7,7 +7,7 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_sciencedirect_response_has_title_and_link():
     response = client.get("/sciencedirect?query=test")
     assert response.status_code == 200
     data = response.json()
@@ -23,6 +23,15 @@ def test_read_main():
         assert 'link' in item
         assert isinstance(item['link'], str)
         assert item['link'].startswith('http')
+
+
+def test_sciencedirect_empty_response_is_empty():
+    response = client.get("/sciencedirect?query=abcdefg+AND+hijklmnop+AND+12345")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data,list)
+
+    assert len(data) is 0   
 
 
 
