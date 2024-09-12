399d1db711f37bd96bbf821cab570498edab2f50
beedawn <bee@macbook-pro.909.vc>
2024-09-11 20:45:22 -0400

    update scopus

--- backend/tests/unit/test_scopus.py
+++ backend/tests/unit/test_scopus.py
@@ -8,7 +8,7 @@ from scopus.scopus import api_key
 client = TestClient(app)
 
 
-def test_sciencedirect_response_has_title_and_link():
+def test_scopus_response_has_title_and_link():
     response = client.get("/scopus?query=test")
     assert response.status_code == 200
     data = response.json()
@@ -26,7 +26,7 @@ def test_sciencedirect_response_has_title_and_link():
         assert item['link'].startswith('http')
 
 
-def test_sciencedirect_empty_response_is_empty():
+def test_scopus_empty_response_is_empty():
     response = client.get("/scopus?query=abcdefg+AND+hijklmnop+AND+12345")
     assert response.status_code == 200
     data = response.json()
