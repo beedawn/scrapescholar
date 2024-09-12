04f5dd393eeadc21091383828d35095cb54abe86
Tristan Hamid <tjh6364@psu.edu>
2024-09-12 00:49:51 -0400

    Completed query builder. Tomorrow, I will work to complete json parser and place search results in classes.

--- backend/sciencedirect/sciencedirect.py
+++ backend/sciencedirect/sciencedirect.py
@@ -1,49 +1,8 @@
-import requests
-import os
-from dotenv import load_dotenv
- 
-
-# Load environment variables from .env file
-load_dotenv()            
-
-#Create QueryParameters class
-class QueryParameters:
-    def __init__(self, keywords = None, subject = None, minYear = None):
-        if keywords is None:
-            keywords = []
-        self.keywords = keywords
-        self.subject = subject
-        self.minYear = minYear
-    def __repr__(self):
-        return (f"QueryParameters(keywords={self.keywords}, "
-                f"subject={self.subject}, minYear={self.minYear})")
-
-#Create SearchResults class
-class SearchResults:
-    def __init__(self, title = None, year = None, citedBy = None, link = None, abstract = None, documentType = None, 
-                 source = None, evaluation = None, methodology = None, clarity = None, completeness = None, transparency = None):
-        self.title = title
-        self.year = year
-        self.citedBy = citedBy
-        self.link = link
-        self.abstract = abstract
-        self.documentType = documentType
-        self.source = source
-        self.evaluation = evaluation
-        self.methodology = methodology
-        self.clarity = clarity
-        self.completeness = completeness
-        self.transparency = transparency
-    def __repr__(self):
-        return (f"SearchResults(title={self.title!r}, year={self.year!r}, citedBy={self.citedBy!r}, "
-                f"link={self.link!r}, abstract={self.abstract!r}, documentType={self.documentType!r}, "
-                f"source={self.source!r}, evaluation={self.evaluation!r}, methodology={self.methodology!r}, "
-                f"clarity={self.clarity!r}, completeness={self.completeness!r}, transparency={self.transparency!r})")
 
 # Triggers for science direct endpoint
 def request_api(query: str):
     #request data from science direct
-    response = requests.get(f"https://api.elsevier.com/content/search/sciencedirect?query={query}&apiKey={api_key}")
+    response = requests.get(f"https://api.elsevier.com/content/search/sciencedirect?httpAccept=application/xml&query={query}&apiKey={api_key}")
     #get json data
     data = response.json()
     #get search-results object
@@ -51,8 +10,6 @@ def request_api(query: str):
     #get entries list
     entries = search_results.get('entry',[])
     #return entries to sciencedirect endpoint response
- 
-    returnList = []
  
     for entry in entries:
         title = entry.get('dc:title')
@@ -64,21 +21,6 @@ def request_api(query: str):
             })
  
     return returnList
-
-
-#   ---main---
-
-
-#  Access an environment variable for API Key
-api_key = os.getenv('SCIENCEDIRECT_APIKEY')
-api_key = "737eab5d80dc68fd8dbb744fcad411b9"    #This is Tristan's API key but should be deleted before merge for security
-
-#   QueryParameters - keywordList is user input, the rest will be static and unique to the scienceDirect parameters
-researcherKeywordList = ["cybersecurity", "AND", "nonprofit"]     
-subject = "COMP"
-minYear = "2015"
-
-#   Build Query
 
 
 
---
+++ backend/scopus/scopus.py
@@ -0,0 +1,90 @@
+import requests
+import os
+import datetime
+# from dotenv import load_dotenv
+ 
+
+# Load environment variables from .env file
+# load_dotenv()            
+
+#Create QueryParameters class
+class QueryParameters:
+    def __init__(self, keywords = None, subject = None, minYear = None):
+        if keywords is None:
+            keywords = []
+        self.keywords = keywords
+        self.subject = subject
+        self.minYear = minYear
+
+#Create SearchResults class
+class SearchResults:
+    def __init__(self, title = None, year = None, citedBy = None, link = None, abstract = None, documentType = None, 
+                 source = None, evaluation = None, methodology = None, clarity = None, completeness = None, transparency = None):
+        self.title = title
+        self.year = year
+        self.citedBy = citedBy
+        self.link = link
+        self.abstract = abstract
+        self.documentType = documentType
+        self.source = source
+        self.evaluation = evaluation
+        self.methodology = methodology
+        self.clarity = clarity
+        self.completeness = completeness
+        self.transparency = transparency
+
+#Create Query Execute Function
+def query_scopus_api(keywords, key, subject = "COMP", minYear = "2015",):
+    #Keyword Builder
+    keywordPhrase = ""
+    for currentWord in keywords:
+        strippedWord = currentWord.strip()
+        if ' ' in currentWord:
+            strippedWordPlus = strippedWord.replace(" ", "+")
+            keywordPhrase += strippedWordPlus
+        else:
+            keywordPhrase += strippedWord
+        keywordPhrase += "%20"
+
+    #Other Parameters
+    httpAccept = "application/json"
+    view = "STANDARD"                               #Note: COMPLETE view is inaccessible with a standard token
+    today = datetime.date.today()
+    currentYear = today.year
+    dateRange = minYear + "-" + str(currentYear)
+    count = "2"
+    sort = "relevancy"
+    subj = subject
+
+    #Final Assembly
+    getPhrase = "https://api.elsevier.com/content/search/scopus?" \
+        + "apiKey=" + key \
+        + "&query=" + keywordPhrase \
+        + "&httpAccept=" + httpAccept \
+        + "&view=" + view \
+        + "&date=" + dateRange \
+        + "&count=" + count \
+        + "&sort=" + sort \
+        + "&subj=" + subject
+    
+    return getPhrase
+
+
+#   ---main--- This is testing the variables that I will recieve from the front end
+
+
+#  Access an environment variable for API Key
+api_key = os.getenv('SCOPUS_APIKEY')
+api_key_th = "737eab5d80dc68fd8dbb744fcad411b9"    #This is Tristan's API key but should be deleted before merge for security
+
+#   QueryParameters - keywordList is user input, the rest will be static and unique to the scienceDirect parameters
+researcherKeywordList = ["cybersecurity", "AND", "non profit", "OR", "charity"]     
+subjectComp = "COMP"
+minYear2015 = "2015"
+
+#   Build Query
+searchQuery = QueryParameters(keywords=researcherKeywordList, subject=subjectComp, minYear=minYear2015)
+queryURL = query_scopus_api(searchQuery.keywords, api_key_th)
+apiResponse = requests.get(queryURL)
+print(apiResponse.json())
+#   Store Results in Class
