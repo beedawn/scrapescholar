c2f8abbb659a9f8a1af3fb366b73c2230c71bf47
beedawn <brandonschmersal@gmail.com>
2024-09-16 16:24:02 -0400

    well-know/acme-challenge kind of working

--- docker/nginx/start_nginx.sh
+++ docker/nginx/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #chomd +x start_nginx.sh before running this container
-envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/templates/app.conf.template > /etc/nginx/conf.d/app.conf
+envsubst '${SERVER_NAME}' < /etc/nginx/templates/app.conf.template > /etc/nginx/conf.d/app.conf
 
 exec nginx -g 'daemon off;'
