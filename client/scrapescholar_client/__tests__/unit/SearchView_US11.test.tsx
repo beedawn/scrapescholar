fc29bec83ddf29305ca392786454140d538c1806
beedawn <brandonschmersal@gmail.com>
2024-09-25 17:52:39 -0400

    update tests

--- client/scrapescholar_client/__tests__/unit/SearchView_US11.test.tsx
+++ client/scrapescholar_client/__tests__/unit/SearchView_US11.test.tsx
@@ -5,76 +5,10 @@ import React from 'react';
 import Dropdown from '../../app/types/DropdownType';
 import { sortResults } from '../../app/components/SearchView/ResultsTable';
 
-/*
+import itemsArray from '../ItemsTestArray';
+import itemsJson from '../ItemsTestJson';
 
-    id: number;
-    title: string;
-    link: string;
-    date: string;
-    source: string;
-    citedby:number;
-    color:string;
-    relevance:number;
-    abstract: string;
-    doctype: string;
-    evaluation_criteria: string;
-    methodology: number;
-    clarity: 0;
-    completeness: 0;
-    transparency: 0;*/
-const items: ResultItem[] = [
-  {
-    id: 0,
-    title: "test 1",
-    link: "link x",
-    date: "2024-05-31",
-    source: "Science Direct",
-    citedby: 0,
-    color: "red",
-    relevance_score: 92,
-    abstract: "a",
-    document_type: "article",
-    evaluation_criteria: "accept",
-    methodology: "0",
-    clarity: "0",
-    transparency: "0",
-    completeness: "0"
-  },
-  {
-    id: 1,
-    title: "test 2",
-    link: "link a",
-    date: "2024-07-01",
-    source: "Scopus",
-    citedby: 1,
-    color: "yellow",
-    relevance_score: 80,
-    abstract: "b",
-    document_type: "journal",
-    evaluation_criteria: "deny",
-    methodology: "1",
-    clarity: "1",
-    transparency: "1",
-    completeness: "1"
-  },
-  {
-    id: 1,
-    title: "test 1",
-    link: "link a",
-    date: "2024-07-01",
-    source: "Scopus",
-    citedby: 1,
-    color: "yellow",
-    relevance_score: 80,
-    abstract: "b",
-    document_type: "journal",
-    evaluation_criteria: "deny",
-    methodology: "1",
-    clarity: "1",
-    transparency: "1",
-    completeness: "1"
-  }
-]
+const items: ResultItem[] = itemsArray;
 
 beforeEach(() => {
   // Reset mocks before each test
@@ -82,44 +16,7 @@ beforeEach(() => {
     Promise.resolve({
       ok: true,
       status: 200,
-      json: () => Promise.resolve(
-        [
-          {
-            "id": "0",
-            "title": "test 1",
-            "link": "link x",
-            "date": "2024-05-31",
-            "source": "Science Direct",
-            "citedby": "0",
-            "color": "red",
-            "relevance_score": "92",
-            "abstract": "a",
-            "document_type": "article",
-            "evaluation_criteria": "accept",
-            "methodology": "0",
-            "clarity": "0",
-            "transparency": "0",
-            "completeness": "0"
-          },
-          {
-            "id": "1",
-            "title": "test 2",
-            "link": "link a",
-            "date": "2024-07-01",
-            "source": "Scopus",
-            "citedby": "1",
-            "color": "yellow",
-            "relevance_score": "80",
-            "abstract": "b",
-            "document_type": "journal",
-            "evaluation_criteria": "deny",
-            "methodology": "1",
-            "clarity": "1",
-            "transparency": "1",
-            "completeness": "1"
-          }
-        ]
-      ),
+      json: () => Promise.resolve(itemsJson),
       headers: new Headers(),
       redirected: false,
       statusText: 'OK',
@@ -173,7 +70,5 @@ describe('SearchView US-11 Component', () => {
       expect(screen.getByText('link x')).toBeInTheDocument()
     }, { timeout: 5000 });
   })
-
- 
 
 });
