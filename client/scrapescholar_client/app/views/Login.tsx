c3093866ee49a9044c1a72120597d9f0f373def8
beedawn <brandonschmersal@gmail.com>
2024-09-25 14:13:38 -0400

    add login

--- client/scrapescholar_client/app/views/Login.tsx
+++ client/scrapescholar_client/app/views/Login.tsx
@@ -12,13 +12,22 @@ interface LoginProps {
 
 
 const Login: React.FC<LoginProps> = ({ setLoggedIn}) => {
-    
+    const [username, setUserName]=useState<string>('');
+    const [password, setPassword]=useState<string>('');
+    const handleLogin = async()=>{
+        setLoggedIn(true)
+        //do something
+    }
     return (
         <div className="flex flex-col sm:flex-row sm:mx-12 justify-center items-center">
             <div className="flex-1 sm:mx-12 w-full flex justify-center">
-            <button onClick={() => { setLoggedIn(true) }} className={"m-5 px-4 py-2 bg-blue-500 text-white rounded "}>
+
+                <form onSubmit={handleLogin}>
+                    <input onChange={(event)=>{setUserName(event.target.value)}} value={username}></input>
+            <button type="submit" className={"m-5 px-4 py-2 bg-blue-500 text-white rounded "}>
             Login
           </button>
+          </form>
             </div>
         </div>
     );
