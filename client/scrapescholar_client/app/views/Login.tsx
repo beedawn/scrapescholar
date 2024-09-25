f4313c818ce6a58502431dfd3a691c996862ebdc
beedawn <brandonschmersal@gmail.com>
2024-09-25 14:21:10 -0400

    add username and password to login

--- client/scrapescholar_client/app/views/Login.tsx
+++ client/scrapescholar_client/app/views/Login.tsx
@@ -16,17 +16,20 @@ const Login: React.FC<LoginProps> = ({ setLoggedIn}) => {
     const [password, setPassword]=useState<string>('');
     const handleLogin = async()=>{
         setLoggedIn(true)
+        alert(username +" "+ password)
         //do something
     }
     return (
-        <div className="flex flex-col sm:flex-row sm:mx-12 justify-center items-center">
+        <div className="flex flex-col mt-40 sm:flex-row sm:mx-12 justify-center items-center">
             <div className="flex-1 sm:mx-12 w-full flex justify-center">
 
                 <form onSubmit={handleLogin}>
-                    <input onChange={(event)=>{setUserName(event.target.value)}} value={username}></input>
-            <button type="submit" className={"m-5 px-4 py-2 bg-blue-500 text-white rounded "}>
+                    <div className="py-3"><label><input onChange={(event)=>{setUserName(event.target.value)}} value={username} placeholder="Username"></input></label></div>
+                    <div className="my-3"><label><input type="password" onChange={(event)=>{setPassword(event.target.value)}} value={password} placeholder="Password"></input></label></div>
+           <div className="flex justify-center"> <button type="submit" className={"m-5 px-4 py-2 bg-blue-500 text-white rounded items-center"}>
             Login
           </button>
+          </div>
           </form>
             </div>
         </div>
