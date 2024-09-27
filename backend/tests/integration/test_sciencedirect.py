ba2fa852010b1e56805d27b84175c5e991af54a2
beedawn <brandonschmersal@gmail.com>
2024-09-27 08:49:23 -0400

    fix tests, should pass

--- backend/tests/integration/test_sciencedirect.py
+++ backend/tests/integration/test_sciencedirect.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from app.main import app
-from sciencedirect.sciencedirect import sciencedirect_api_key
+from api_tools.api_tools import sciencedirect_api_key
 
 client = TestClient(app)
 
 
 def test_sciencedirect_response_has_title_and_link():
-    response = client.get("/sciencedirect?query=test")
+    response = client.get("/academic_data?keywords=test&academic_database=ScienceDirect")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
@@ -21,7 +21,7 @@ def test_sciencedirect_response_has_title_and_link():
 
 
 def test_sciencedirect_empty_response_is_empty():
-    response = client.get("/sciencedirect?query=abcdefg+AND+hijklmnop+AND+12345")
+    response = client.get("/academic_data?keywords=abcdefg+AND+hijklmnop+AND+12345&academic_database=ScienceDirect")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
--- backend/tests/unit/test_sciencedirect.py
+++ backend/tests/unit/test_sciencedirect.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 
 def test_sciencedirect_response_has_title_and_link():
-    response = client.get("/academic_data?keywords=test&academic_database=Science%20Direct")
+    response = client.get("/academic_data?keywords=test&academic_database=ScienceDirect")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
@@ -21,7 +21,7 @@ def test_sciencedirect_response_has_title_and_link():
 
 
 def test_sciencedirect_empty_response_is_empty():
-    response = client.get("/academic_data?keywords=abcdefg+AND+hijklmnop+AND+12345&academic_database=Science%20Direct")
+    response = client.get("/academic_data?keywords=abcdefg+AND+hijklmnop+AND+12345&academic_database=ScienceDirect")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
