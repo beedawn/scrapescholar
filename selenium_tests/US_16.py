045b6895d3e54fe7f6cf8118ecfb02bebc53c927
beedawn <brandonschmersal@gmail.com>
2024-10-16 06:57:49 -0400

    UT-16.2

--- selenium_tests/US_16.py
+++ selenium_tests/US_16.py
@@ -51,3 +51,47 @@ def test_login_us_16():
         print(driver.title)
         print(driver.current_url)   
         driver.quit()
+
+
+
+def test_bad_login_us_16():
+    try:
+        # Initialize the WebDriver
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    
+        # Navigate to the login page
+        driver.get("http://0.0.0.0:3000/")
+        driver.maximize_window()
+    
+        # Locate and interact with the login elements
+        username_field = driver.find_element(By.NAME, 'username_input')
+        password_field = driver.find_element(By.NAME, 'password_input')
+        login_button = driver.find_element(By.NAME, 'login_button')
+        time.sleep(1)
+
+        # Enter login credentials and submit the form
+        username_field.send_keys("bad")
+        password_field.send_keys("bad")
+        login_button.click()
+
+        #await navbar to populate
+        initial_page = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='navbar']"))
+     
+        )
+
+        invalid_login= WebDriverWait(driver, 10).until(
+            EX.presence_of_element_located(By.XPATH, ("//*[contains(text(), 'Invalid Login')]"))
+            )
+        assert initial_page is None
+        assert invalid_login 
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    
+    finally:
+        # Close the WebDriver
+        time.sleep(1)
+        print(driver.title)
+        print(driver.current_url)   
+        driver.quit()
