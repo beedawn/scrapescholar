47d23e7e6c462cf4c06735398de528eb30fc69c0
beedawn <brandonschmersal@gmail.com>
2024-10-17 19:02:36 -0400

    try again

--- selenium_tests/US_16.py
+++ selenium_tests/US_16.py
@@ -107,10 +107,15 @@ def test_login_us_16_with_refresh():
         driver.maximize_window()
     
         # Locate and interact with the login elements
-        username_field = driver.find_element(By.NAME, 'username_input')
-        password_field = driver.find_element(By.NAME, 'password_input')
-        login_button = driver.find_element(By.NAME, 'login_button')
-        time.sleep(1)
+        username_field = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.NAME, 'username_input'))
+            )
+        password_field = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.NAME, 'password_input'))
+        )
+        login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, 'login_button'))
+        )
 
         # Enter login credentials and submit the form
         username_field.send_keys(testuser)
