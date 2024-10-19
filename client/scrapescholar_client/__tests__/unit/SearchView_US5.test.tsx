6e3e71914a8afe0805563d50be7230e5f4a26363
beedawn <brandonschmersal@gmail.com>
2024-10-18 20:40:44 -0400

    fix test names

--- client/scrapescholar_client/__tests__/unit/SearchView_US5.test.tsx
+++ client/scrapescholar_client/__tests__/unit/SearchView_US5.test.tsx
@@ -20,7 +20,7 @@ afterEach(() => {
 describe('SearchView US-5 Component', () => {
     const mockSetLoggedIn = jest.fn();
     const testInput = "test input"
-    test('US-4 check relevancy column contains relevancy', () => {
+    test('US-5 UT-5.1 check relevancy column contains relevancy', () => {
         render(<SearchView setLoggedIn={mockSetLoggedIn} />);
         submitSearch(testInput)
 
@@ -29,7 +29,7 @@ describe('SearchView US-5 Component', () => {
             expect(rows[0].children[8].textContent).toContain(/Relevancy/i);
         }, { timeout: 5000 })
     });
-    test('US-4 UT-5.3 check relevancy column contains Relevant, SemiRelevant, and Not Relevant when expanded', () => {
+    test('US-5 UT-5.3 check relevancy column contains Relevant, SemiRelevant, and Not Relevant when expanded', () => {
         render(<SearchView setLoggedIn={mockSetLoggedIn} />);
         submitSearch(testInput)
 
@@ -42,7 +42,7 @@ describe('SearchView US-5 Component', () => {
             expect(rows[0].children[8].textContent).toContain(/Not Relevant/i);
         }, { timeout: 5000 })
     });
-    test('US-4 UT-5.3 check relevancy column contains Relevant, SemiRelevant, and Not Relevant when expanded', () => {
+    test('US-5 UT-5.3 check relevancy column contains Relevant, SemiRelevant, and Not Relevant when expanded, and closes when close is pressed', () => {
         render(<SearchView setLoggedIn={mockSetLoggedIn} />);
         submitSearch(testInput)
 
@@ -66,7 +66,7 @@ describe('SearchView US-5 Component', () => {
         expect(rows[0].children[8].textContent).toContain(/Relevancy/i);
     }, { timeout: 5000 })
     });
-    test('US-4 UT-5.1 check relevancy column contains Relevant, SemiRelevant, and Not Relevant when expanded', () => {
+    test('US-5 UT-5.1 check relevancy values can be clicked when expanded', () => {
         
         render(<SearchView setLoggedIn={mockSetLoggedIn} />);
         submitSearch(testInput)
