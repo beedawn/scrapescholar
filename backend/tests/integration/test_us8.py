c9ccd9fe9337c6bece69db91f5ec08657b08ee4a
beedawn <brandonschmersal@gmail.com>
2024-10-30 06:21:35 -0400

    UT-8.2

--- backend/tests/integration/test_us8.py
+++ backend/tests/integration/test_us8.py
@@ -5,8 +5,12 @@ from app.db.session import SessionLocal
 from tests.integration.tools.get_cookie import get_cookie
 from tests.integration.tools.base_url import base_url
 from academic_databases.SearchResult import SearchResult
+from io import StringIO
+import csv
 
 session = get_cookie()
+
+
 @pytest.fixture
 def test_db_session():
     """Fixture to provide a database session for testing"""
@@ -19,29 +23,33 @@ def test_db_session():
 
 #UT-8.2
 def test_generate_data(test_db_session: Session):
-
-
     api_query = "test"
     query_string = "&academic_database=Scopus&academic_database=ScienceDirect"
     search_request = session.get(f"{base_url}/academic_data?keywords={api_query}{query_string}")
-    search_request =search_request.json()
-    search_id=search_request["search_id"]
+    search_request = search_request.json()
+    search_id = search_request["search_id"]
     articles = search_request["articles"]
     search_articles = []
     for item in articles:
         search_articles.append(SearchResult(**item))
-    chunks = list(csv_generator(search_articles, test_db_session))
+    #join generator into string
+    csv_result = ""
+    for chunk in csv_generator(search_articles, test_db_session):
+        csv_result += chunk
+    #build field names
     fieldnames = build_fieldnames(search_articles)
-    final_result = fieldnames
-    for item in search_articles:
-        row = add_user_comments_to_list(item, test_db_session)
-        print(row)
-        final_result.append(row)
+    #generate csv
+    buffer = StringIO()
+    writer = csv.DictWriter(buffer, fieldnames=fieldnames)
+    writer.writeheader()
+    #write csv
+    for row in search_articles:
+        #inject comments and usernames
+        row_data = add_user_comments_to_list(row, test_db_session)
+        writer.writerow(row_data)
 
-    print("-----------------CHUNKS---------")
-    print(chunks)
-    print("-----------------FINAL RESULT---------")
-    print(final_result)
-    # Verify the output matches expected chunks
-    # assert chunks == ["chunk 0\n", "chunk 1\n", "chunk 2\n", "chunk 3\n", "chunk 4\n"]
+    test_result = buffer.getvalue()
+    buffer.close()
+
+    assert csv_result == test_result
     session.delete(f"{base_url}/search/user/search/title?search_id={search_id}")
