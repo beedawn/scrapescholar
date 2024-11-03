a06b45d4e539fc30e85f2f79a017468fd4f57a06
beedawn <brandonschmersal@gmail.com>
2024-11-03 08:09:47 -0500

    tests fixed

--- backend/tests/database/test_crud_user_data.py
+++ backend/tests/database/test_crud_user_data.py
@@ -5,9 +5,12 @@ from sqlalchemy.orm import Session
 from fastapi.exceptions import HTTPException
 from app.crud.user_data import get_user_data, create_user_data, update_user_data, delete_user_data
 from app.crud.article import create_article, delete_article
+from app.crud.search import create_search, delete_search
 from app.schemas.user_data import UserDataUpdate
 from app.schemas.article import ArticleCreate
+from app.schemas.search import SearchCreate
 from app.db.session import SessionLocal
+from app.crud.user import create_user, get_user_by_username
 
 @pytest.fixture
 def test_db_session():
@@ -31,36 +34,41 @@ mock_article_data = {
     "doi": "10.1000/testdoi"
 }
 def setup(test_db_session):
+    test_user = get_user_by_username(test_db_session, "testuser")
+    search = SearchCreate(user_id=test_user.user_id, search_keywords=None, Status=None, title="test search")
+    created_search = create_search(test_db_session, search)
+    mock_article_data.update({"search_id": created_search.search_id})
     article_in = ArticleCreate(**mock_article_data)
-    created_article = create_article(test_db_session, article_in, user_id=1)
+    created_article = create_article(test_db_session, article_in, user_id=test_user.user_id)
 
     created_user_data = create_user_data(
         test_db_session,
-        user_id=1,
+        user_id=test_user.user_id,
         article_id=created_article.article_id
     )
-    return created_article, created_user_data
-def teardown(test_db_session, created_article, created_user_data):
+    return created_article, created_user_data, created_search
+def teardown(test_db_session, created_article, created_user_data, created_search):
     delete_user_data(test_db_session, created_user_data.userdata_id)
     delete_article(test_db_session, created_article.article_id)
+    delete_search(test_db_session, created_search.search_id)
 
 def test_create_user_data(test_db_session: Session):
     """Test creating a new user data entry."""
-
-    created_article, created_user_data = setup(test_db_session)
-    assert created_user_data.user_id == 1
+    test_user = get_user_by_username(test_db_session, "testuser")
+    created_article, created_user_data, created_search = setup(test_db_session)
+    assert created_user_data.user_id == test_user.user_id
     assert created_user_data.article_id == created_article.article_id
-    teardown(test_db_session, created_article, created_user_data)
+    teardown(test_db_session, created_article, created_user_data, created_search)
 
 def test_get_user_data(test_db_session: Session):
     """Test retrieving a user data entry by article_id."""
 
-    created_article, created_user_data = setup(test_db_session)
+    created_article, created_user_data, created_search = setup(test_db_session)
 
     # Retrieve the UserData entry
     fetched_user_data = get_user_data(test_db_session, article_id=created_user_data.article_id)
     assert fetched_user_data.article_id == created_user_data.article_id
-    teardown(test_db_session, created_article, created_user_data)
+    teardown(test_db_session, created_article, created_user_data, created_search)
 
 def test_get_user_data_not_found(test_db_session: Session):
     """Test error handling when a user data entry is not found by article_id."""
@@ -71,7 +79,7 @@ def test_get_user_data_not_found(test_db_session: Session):
 @pytest.mark.asyncio
 async def test_update_user_data(test_db_session: Session):
     """Test updating an existing user data entry."""
-    created_article, created_user_data = setup(test_db_session)
+    created_article, created_user_data, created_search = setup(test_db_session)
 
     # Update the user data entry with integer values
     update_data = UserDataUpdate(
@@ -90,7 +98,7 @@ async def test_update_user_data(test_db_session: Session):
     assert updated_user_data.clarity == 5
     assert updated_user_data.transparency == 4
     assert updated_user_data.completeness == 5
-    teardown(test_db_session, created_article, created_user_data)
+    teardown(test_db_session, created_article, created_user_data, created_search)
 
 
 @pytest.mark.asyncio
