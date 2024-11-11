1fda6766df9089e1d027eb05727331d4e2c67b2a
Tristan Hamid <tjh6364@psu.edu>
2024-11-11 11:31:11 -0500

    Working prototype. Still need to refine percentages and believe it has something to do with cleaning punctuation more

--- backend/algorithm/algorithmTH.py
+++ backend/algorithm/algorithmTH.py
@@ -7,6 +7,7 @@ import string
 
 dotenv.load_dotenv()
 thesaurus_api_key = os.getenv('THESAURUS_APIKEY')
+dictionary_api_key = os.getenv('DICTIONARY_APIKEY')
 # file_path = os.path.join(os.path.dirname(__file__), 'words_dictionary.json')
 # print("File Path")
 # print(file_path)
@@ -15,7 +16,7 @@ thesaurus_api_key = os.getenv('THESAURUS_APIKEY')
 #     data = json.load(f)
 
 def check_limit(word, set_of_words):
-    if len(word) > 3 and word not in set_of_words:                  ##### do you even need this loop?
+    if len(word) >= 3 and word not in set_of_words:                  ##### do you even need this loop?
         set_of_words.add(word)
         print("Set of Words")
         print(set_of_words)
@@ -23,7 +24,7 @@ def check_limit(word, set_of_words):
 
 def break_word_into_possible_words(word, set_of_words):
     for i in range(len(word)):
-        if i+3 < len(word):
+        if i+3 < len(word)+1:                                             ##### i + 3 saves running the first two iterations of the loop
             #adds to right side by adding characters to end of word
             print("Break word into poss keywords loop:" + str(i))
             new_word = word[:i+3]                                       ###### i + 1 saves one loop
@@ -31,8 +32,8 @@ def break_word_into_possible_words(word, set_of_words):
             check_limit(new_word, set_of_words)
 
         # trims from left side, by moving inward
-        # new_word = word[i:]
-        # check_limit(new_word, set_of_words)
+        new_word = word[i:]
+        check_limit(new_word, set_of_words)
 
         # for j in range(i + 1, len(word) + 1):
         #     substring = word[i:j]
@@ -54,28 +55,6 @@ def calc_score(score, text_list):
         return round(score / weighted_sum * 100, 2)
     else:
         return round(score / len(text_list) * 100, 2)
-
-
-#add new function or modify this one to just break down each word and return a list of the substrings
-def score_word(word, synonyms_list, keywords_sliced_list, keyword_list):
-    score = 0
-    #need a way to calculate weight based off total words in text for each score incrementation
-    for keyword in keyword_list:
-        if word == keyword:
-            score += 1
-
-    for keyword in keywords_sliced_list:
-        if word == keyword:
-            score += 0.75
-
-    for synonym in synonyms_list:
-        if word == synonym:
-            score += 0.5
-
-        elif word in synonym:
-            score += 0.25
-
-    return score
 
 
 def api_request(keyword):
@@ -107,17 +86,19 @@ def flatten_list(word_list):
     flat_list = list(itertools.chain.from_iterable(word_list))
     return flat_list
 
+
 def remove_punctuation(text):
     translator = str.maketrans('', '', string.punctuation)
     return text.translate(translator)
+
 
 def algorithm(text, keywords):
     score = 0
     keyword_list = keywords.split()
     print("Keyword List")
     print(keyword_list)
-    no_punctuation_text_list = remove_punctuation(text)
-    text_list = no_punctuation_text_list.split()
+    no_punctuation_text = remove_punctuation(text)
+    text_list = no_punctuation_text.split()
     print("Text List")
     print(text_list)
     synonyms_list = set()
@@ -149,6 +130,69 @@ def algorithm(text, keywords):
     score = calc_score(score, text_list)
 
     return score
+
+
+def clean(text):
+    text_list = set()   
+    no_punctuation_text = remove_punctuation(text)
+    text_list_no_punct = no_punctuation_text.split()
+    for text_item in text_list_no_punct:
+        if len(text_item) > 3:
+            text_list.add(text_item.lower())
+    print("Text List")
+    print(text_list)
+    return(text_list)
+
+
+def compare(text, keywords):
+    num_similar = 0
+    for item in text:
+        if item in keywords:
+            num_similar += 1
+    return([num_similar, len(text)])
+
+def get_stems(keywords):
+    score = 0
+    
+    keyword_stems = set()
+
+    keyword_list = keywords.split()
+    for each_item in keyword_list:
+        keyword_stems.add(each_item)
+    print("Keyword List")
+    print(keyword_stems)
+
+
+    # keywords_list = set()
+    # similar_list = set()
+    
+    # keyword_definition = set()
+
+    for word in keyword_list:
+        print("word = " + word)
+        response = requests.get("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"+word+"?key="+thesaurus_api_key)
+        responseJSON = response.json()
+        # print(responseJSON)
+        for item in responseJSON:
+            if 'meta' in item and 'stems' in item['meta']:
+                for metaStem in item['meta']['stems']:
+                    if len(metaStem) > 3:
+                        keyword_stems.add(metaStem)
+            else:
+                if len(item) > 3:
+                    keyword_stems.add(item)
+        # definition = str(responseJSON[0]['shortdef']).split()
+        #for word in definition:
+        #    if len(word) > 3:
+        #       keyword_definition.add(word)
+    print("Keyword Stems:")
+    print(keyword_stems)
+    return(keyword_stems)
+    # print("Keyword Definition:")
+    # print(keyword_definition)
+
+def score_word():
+    return
 
 #
 # if __name__ == '__main__':
--- backend/algorithm/algorithm_interfaceTH.py
+++ backend/algorithm/algorithm_interfaceTH.py
@@ -1,30 +1,44 @@
 import time
 #gets article title, article abstract, author keywords
 from algorithmTH import algorithm
+from algorithmTH import get_stems
+from algorithmTH import clean
+from algorithmTH import compare
 
-keywords = "cool"
-article_title = "Cybersecurity is cool!"
-# article_title = "The proliferation of information and communication technologies in enterprises enables them to develop new business models and enhance their operational and commercial activities. Nevertheless, this practice also introduces new cybersecurity risks and vulnerabilities. This may not be an issue for large organizations with the resources and mature cybersecurity programs in place; the situation with small and medium-sized enterprises (SMEs) is different since they often lack the resources, expertise, and incentives to prioritize cybersecurity. In such cases, cybersecurity awareness can be a critical component of cyberdefense. However, research studies dealing with cybersecurity awareness or related domains exclusively for SMEs are rare, indicating a pressing need for research addressing the cybersecurity awareness requirements of SMEs. Prior to that, though, it is crucial to identify which aspects of cybersecurity awareness require further research in order to adapt or conform to the needs of SMEs. In this study, we conducted a systematic literature review that focused on cybersecurity awareness, prioritizing those performed with a particular focus on SMEs. The study seeks to analyze and evaluate such studies primarily to determine knowledge and research gaps in the cybersecurity awareness field for SMEs, thus providing a direction for future research."
-article_abstract = None
+keywords = "cybersecurity training awareness small organizations vulnerabilities risk"
+article_title = "A quest for research and knowledge gaps in cybersecurity awareness for small and medium-sized enterprises"
+article_abstract = "The proliferation of information and communication technologies in enterprises enables them to develop new business models and enhance their operational and commercial activities. Nevertheless, this practice also introduces new cybersecurity risks and vulnerabilities. This may not be an issue for large organizations with the resources and mature cybersecurity programs in place; the situation with small and medium-sized enterprises (SMEs) is different since they often lack the resources, expertise, and incentives to prioritize cybersecurity. In such cases, cybersecurity awareness can be a critical component of cyberdefense. However, research studies dealing with cybersecurity awareness or related domains exclusively for SMEs are rare, indicating a pressing need for research addressing the cybersecurity awareness requirements of SMEs. Prior to that, though, it is crucial to identify which aspects of cybersecurity awareness require further research in order to adapt or conform to the needs of SMEs. In this study, we conducted a systematic literature review that focused on cybersecurity awareness, prioritizing those performed with a particular focus on SMEs. The study seeks to analyze and evaluate such studies primarily to determine knowledge and research gaps in the cybersecurity awareness field for SMEs, thus providing a direction for future research."
 authorKeywords = None
 
 
 def algorithm_interface(keywords, article_title, article_abstract=None, authorkeywords=None):
-    title_score = algorithm(article_title, keywords)
+    # relevance_score = dictionaryAlgorithm(article_title, keywords)
     relevance_score = 0
+    keyword_stems = get_stems(keywords)
+    
     if article_title is not None and article_abstract is not None:
-        abstract_score = algorithm(article_title, keywords)
-        relevance_score = (title_score + abstract_score) / 2
+        cleaned_title = clean(article_title)
+        cleaned_abstract = clean(article_abstract)
+        title_score = compare(cleaned_title, keyword_stems)
+        abstract_score = compare(cleaned_abstract, keyword_stems)
+        print("titel score")
+        print(title_score)
+        print("abstract score")
+        print(abstract_score)
+        relevance_score = (title_score[0] + abstract_score[0]) / (title_score[1] + abstract_score[1])
+        return relevance_score
     if article_title is not None and article_abstract is None:
-        relevance_score = title_score
-
-    return round(relevance_score * (100/325), 0)
-#325 is the max score returned by algorithm
+        cleaned_title = clean(article_title)
+        title_score = compare(cleaned_title, keyword_stems)
+        relevance_score = title_score[0] / title_score[1]
+        print("title score")
+        print(title_score)
+        return(relevance_score)
 
 start = time.time()
 result = algorithm_interface(keywords, article_title, article_abstract, authorKeywords)
 end = time.time()
-print("Relevance Score: " + str(result))
+print("Relevance Score: " + str(round(result*100, 2)) + "%")
 
 timeAlgorithm = end - start
 print("Algorithm Time: " + str(round(timeAlgorithm,4)) + " seconds")
