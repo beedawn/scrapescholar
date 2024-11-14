1681734358879b4398ecad6bf0b1b4ba25d98e21
beedawn <brandonschmersal@gmail.com>
2024-11-13 19:38:09 -0500

    try again

--- backend/tests/unit/test_main.py
+++ backend/tests/unit/test_main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from tests.integration.tools.get_cookie import get_cookie
 from app.main import app
-
+from tests.integration.tools.base_url import base_url
 client = TestClient(app)
 session = get_cookie()
 
@@ -13,13 +13,13 @@ def test_read_main():
     assert response.json() == {"message": "Hello World"}
 
 def test_sciencedirect_present_in_sources():
-    response = session.get("/academic_sources")
+    response = session.get(f"{base_url}/academic_sources")
     assert response.status_code == 200
     data = response.json()
     assert "ScienceDirect" in data
 
 def test_scopus_present_in_sources():
-    response = session.get("/academic_sources")
+    response = session.get(f"{base_url}/academic_sources")
     assert response.status_code == 200
     data = response.json()
     assert "Scopus" in data
