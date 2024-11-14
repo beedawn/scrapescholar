6ed2639c8b68cca1426e707a9e4d5407c63f0b07
beedawn <brandonschmersal@gmail.com>
2024-11-13 19:52:22 -0500

    update again

--- backend/tests/unit/test_main.py
+++ backend/tests/unit/test_main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from tests.integration.tools.get_cookie import get_cookie
 from app.main import app
-from tests.integration.tools.base_url import base_url
+
 client = TestClient(app)
-session = get_cookie()
+
 
 def test_read_main():
     response = client.get("/health_check")
