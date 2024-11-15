3f299837c6748860dbdc6d1aa891184728e9040b
beedawn <brandonschmersal@gmail.com>
2024-11-14 21:30:06 -0500

    fix algorithm

--- backend/algorithm/algorithm_interface.py
+++ backend/algorithm/algorithm_interface.py
@@ -3,6 +3,24 @@ from algorithm.algorithm import algorithm
 
 
 def algorithm_interface(keywords, article_title, article_abstract=None):
+    keywords = keywords.split(" ")
+    skip_next = False
+    new_keywords = []
+    #filters out NOT, AND, OR and skips words after NOT to make algorithm more accurate and faster
+    for keyword in keywords:
+        if skip_next:
+            skip_next = False
+            continue
+
+        elif keyword == "NOT":
+            skip_next = True
+            continue
+        elif keyword == "AND":
+            continue
+        elif keyword == "OR":
+            continue
+        new_keywords.append(keyword)
+    keywords = " ".join(new_keywords)
     title_score = algorithm(article_title, keywords)
     relevance_score = 0
     if article_title is not None and article_abstract is not None:
@@ -11,5 +29,5 @@ def algorithm_interface(keywords, article_title, article_abstract=None):
     if article_title is not None and article_abstract is None:
         relevance_score = title_score
 
-    return round(relevance_score * (100/325), 0)
+    return round(relevance_score * (100 / 325), 0)
 #325 is the max score returned by algorithm
