3505c52d89c37d597b7eb636c74263e7d2b04f35
beedawn <brandonschmersal@gmail.com>
2024-11-18 20:04:25 -0500

    more cleaning

--- backend/api_tools/api_tools.py
+++ backend/api_tools/api_tools.py
@@ -1,20 +1,5 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Access an environment variable
-scopus_api_key = os.getenv('SCOPUS_APIKEY')
-sciencedirect_api_key = os.getenv('SCIENCEDIRECT_APIKEY')
-scopus_inst_token = os.getenv('SCOPUS_INSTTOKEN')
-
-
 def parse_data_scopus(response):
-    #get json data
     data = response.json()
-    #get search-results object
     search_results = data.get('search-results', {})
-    #get entries list
     articles = search_results.get('entry', [])
     return articles
