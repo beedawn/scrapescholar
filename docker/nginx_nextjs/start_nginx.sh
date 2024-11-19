4d745130338e7b53719244bb748ab9a1586da772
beedawn <brandonschmersal@gmail.com>
2024-11-19 18:49:31 -0500

    working with port 80...

--- docker/nginx_nextjs/start_nginx.sh
+++ docker/nginx_nextjs/start_nginx.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 
-# Substitute environment variables in the NGINX template
+
 envsubst '\$NGINX_HOST \$NGINX_PORT' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Print the generated configuration (for debugging purposes)
-echo "Generated NGINX config:"
-cat /etc/nginx/conf.d/default.conf
 
+echo "Generated NGINX config:"
+cat /etc/nginx/conf.d/nginx.conf
+
+
+echo "nginx conf"
+cat /etc/nginx/conf.d/nginx.conf
 # Start NGINX in the foreground (daemon off)
 nginx -g 'daemon off;'
