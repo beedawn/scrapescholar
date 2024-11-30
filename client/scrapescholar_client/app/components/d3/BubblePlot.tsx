d1b64ae63da31bd8ad818d5e6bb0a7df04ecee63
beedawn <brandonschmersal@gmail.com>
2024-11-30 08:34:08 -0500

    bubbleplot fixed

--- client/scrapescholar_client/app/components/d3/BubblePlot.tsx
+++ client/scrapescholar_client/app/components/d3/BubblePlot.tsx
@@ -1,6 +1,4 @@
-
-
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 
 interface BubblePlotData {
@@ -10,6 +8,7 @@ interface BubblePlotData {
   color: string,
   label: string,
 }
+
 interface BubblePlotProps {
   data: BubblePlotData[],
   width2?: number,
@@ -21,53 +20,42 @@ interface BubblePlotProps {
   className?: string;
 }
 
-const BubblePlot: React.FC<BubblePlotProps> = ({ data}) => {
+const BubblePlot: React.FC<BubblePlotProps> = ({ data }) => {
   useEffect(() => {
-    // set the dimensions and margins of the graph
-    const margin = { top: 50, right: 20, bottom: 30, left: 50 },
-      width = 350 - margin.left - margin.right,
-      height = 300 - margin.top - margin.bottom;
+    // Set the dimensions and margins of the graph
+    const margin = { top: 50, right: 100, bottom: 30, left: 50 };
+    const width = 350 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
 
     const svg = d3.select("#bubbleplot")
       .append("svg")
-      .attr("width", width)
+      .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-    let xMax = d3.max(data, d => d.x) ?? 0;
-    let x = d3.scaleLinear().domain([0, xMax])
-      .range([0, width - 100]);
-    let yMax = d3.max(data, d => d.y) ?? 0;
-    let y = d3.scaleLinear().domain([0, yMax]).range([height - 100, 20]);
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Add a scale for bubble size
-    let zMax = d3.max(data, d => d.x) ?? 0;
-    const z = d3.scaleLinear()
-      .domain([0, zMax])
-      .range([1, 30]);
+    const xMax = d3.max(data, d => d.x) ?? 0;
+    const x = d3.scaleLinear().domain([0, xMax]).range([0, width]);
 
-    // Define force simulation
+    const yMax = d3.max(data, d => d.y) ?? 0;
+    const y = d3.scaleLinear().domain([0, yMax]).range([height, 0]);
+
+    const zMax = d3.max(data, d => d.radius) ?? 0;
+    const z = d3.scaleLinear().domain([0, zMax]).range([5, 30]);
+
     const simulation = d3.forceSimulation(data)
-      .force("x", d3.forceX((d, i) => x(i)).strength(0.2))
-      .force("y", d3.forceY(d => y(d.y ?? 0)).strength(0.2))
-      .force("collide", d3.forceCollide(d => z(d.x)))
+      .force("x", d3.forceX(d => x(d.x)).strength(0.1))
+      .force("y", d3.forceY(d => y(d.y)).strength(0.1))
+      .force("collide", d3.forceCollide(d => z(d.radius)))
       .on("tick", ticked);
-      
-    // Add bubbles
     const bubbles = svg.append('g')
-      .selectAll("dot")
+      .selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", function (d, i) { return (i) })
-      .attr("cy", function (d, i) {
-        return (i < 6) ? 50 : 150;
-
-      })
-      .attr("r", function (d) { return d.radius })
-      .style("fill", function (d) { return d.color })
-      .style("opacity", "1")
+      .attr("r", d => z(d.radius))
+      .style("fill", d => d.color)
+      .style("opacity", 0.8);
 
     const labels = svg.selectAll("text")
       .data(data)
@@ -80,25 +68,24 @@ const BubblePlot: React.FC<BubblePlotProps> = ({ data}) => {
       .text(d => d.label)
       .style("fill", "white")
       .style("font-size", "12px")
-      .style("font-family", "sans-serif")
+      .style("font-family", "sans-serif");
 
     function ticked() {
-      simulation.tick(300);
-      bubbles.attr("cx", (d => d.x))
+      bubbles.attr("cx", d => d.x)
         .attr("cy", d => d.y);
+
+      labels.attr("x", d => d.x)
+        .attr("y", d => d.y);
     }
+
     return () => {
       d3.select("#bubbleplot").selectAll("*").remove();
-    }
+    };
   }, [data]);
 
   return (
-    <div id="bubbleplot" data-testid="bubble_plot" className="bg-slate-600">
-    </div>
+    <div id="bubbleplot" data-testid="bubble_plot" className={"bg-slate-600"}/>
   );
 };
 
 export default BubblePlot;
-
-
-
